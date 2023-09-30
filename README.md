a981bf37ec28b516cad8e58147d723d449858d91
Na-Eng-Web <sonwatkar777@gmil.com>
2023-10-01 02:43:09 +0530

    added filter

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Backend-for-Trains
+
+To start project
+first add your db link in server.js
+run script npm run gen-data
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 app.use(cors());
+app.use(express.json());
 
 mongoose
   .connect("mongodb://localhost:27017/train_search")
@@ -20,21 +21,60 @@ const trainsSchema = new mongoose.Schema({
 
 const Trains = mongoose.model("trains", trainsSchema);
 
-app.use(express.json());
-
-app.post("/api", (req, res) => {
+app.post("/search", (req, res) => {
   const { From, To } = req.body;
   console.log(From, To);
   Trains.find({
     route: { $all: [From, To] },
   })
     .then((data) => {
-      res.json(data);
+      const modifiedTrains = data.filter((train) => {
+        const fromIndex = train.route.indexOf(From);
+        const toIndex = train.route.indexOf(To);
+        return fromIndex !== -1 && toIndex !== -1 && fromIndex < toIndex;
+      });
+
+      const modifiedTrainsWithTimeDifference = modifiedTrains.map((train) => {
+        const fromIndex = train.route.indexOf(From);
+        const toIndex = train.route.indexOf(To);
+        const fromTime = parseTime(train.time[fromIndex]);
+        const toTime = parseTime(train.time[toIndex]);
+        const timeDifferenceInMinutes = calculateTimeDifference(
+          fromTime,
+          toTime
+        );
+
+        const hours = Math.floor(timeDifferenceInMinutes / 60);
+        const minutes = timeDifferenceInMinutes % 60;
+        const formattedTimeDifference = `${String(hours).padStart(
+          2,
+          "0"
+        )}:${String(minutes).padStart(2, "0")}`;
+
+        return {
+          ...train._doc,
+          time: formattedTimeDifference,
+        };
+      });
+
+      res.json(modifiedTrainsWithTimeDifference);
     })
     .catch((err) => {
       console.log(err);
       res.status(500).json({ error: "Internal Server Error" });
     });
 });
+
+function parseTime(timeString) {
+  const [hours, minutes] = timeString.split(":");
+  return { hours: parseInt(hours), minutes: parseInt(minutes) };
+}
+
+function calculateTimeDifference(fromTime, toTime) {
+  const fromMinutes = fromTime.hours * 60 + fromTime.minutes;
+  const toMinutes = toTime.hours * 60 + toTime.minutes;
+  const timeDifference = toMinutes - fromMinutes;
+  return Math.abs(timeDifference);
+}
 
 app.listen(4000, () => console.log("Server is live at 4000!"));
